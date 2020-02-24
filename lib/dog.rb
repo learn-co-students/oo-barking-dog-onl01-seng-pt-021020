7c846cf8427121204a4f92df1a54580ace769e06
Kyle Imhoff <imhoff.kyle@gmail.com>
2020-02-24 04:58:20 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+class Dog 
+  def name=(dog_name)
+    @doggo = dog_name
+  end
+  
+  def name 
+    @doggo
+  end
+   def bark
+     puts "woof!"
+   end
+ end
+ 
+ Fido = Dog.new 
+ Fido.name = "Fido"
